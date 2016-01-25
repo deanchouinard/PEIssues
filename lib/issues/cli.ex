b8ee99b111b661a2fd79f9aef15fafd602b2741d
Dean Chouinard <deanchouinard@gmail.com>
2016-01-25 10:15:58 -0500

    Take first n items

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -2,12 +2,49 @@ defmodule Issues.CLI do
   @default_count 4
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> convert_to_list_of_maps
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+      fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
+  end
+
+  def convert_to_list_of_maps(list) do
+    list
+    |> Enum.map(&Enum.into(&1, Map.new))
+  end
+
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
   end
 
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [ help: :boolean],
                                     aliases: [ h:   :help])
+                                    # IO.puts parse
 
     case parse do
       { [ help: true ], _, _ }
